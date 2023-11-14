5f100e5a4145c8b377879f98c6856009f22ea591
Jonathan Eanes <jdeanes0@frostburg.edu>
2023-11-14 18:12:46 -0500

    Began framework for P0010

---
+++ 310/w13/P0010/projectmain.py
@@ -0,0 +1,17 @@
+"""
+Driver code for P0010, the one about hash maps.
+
+@author jdeanes0
+@version 11/14/23
+"""
+
+from structures import hashmap
+
+def main():
+    """
+    Main function in the project, controls the program flow by initializing the data structure
+    and beginning the user-driven loop.
+    """
+
+if __name__ == "__main__":
+    main()

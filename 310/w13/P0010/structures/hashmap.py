6e1c6183bc03981085d870cf77f176ca9d2f9c46
jdeanes0 <jdeanes0@frostburg.edu>
2023-11-20 11:53:00 -0500

    Kinda just changed stuff?

--- 310/w13/P0010/structures/hashmap.py
+++ 310/w13/P0010/structures/hashmap.py
@@ -25,12 +25,31 @@ class HashMap:
 
     def add(self, r: Record):
         """Adds a Record to the table"""
-        self.hash(r.get_movie())
+        self.table[self.hash(r.get_movie())] = r
         self.count += 1
 
     def hash(self, title: str) -> int:
         """
-        Hashes the title of the movie
+        Hashes the first two words of the title of the movie
+        """
+        # Get the first two tokens of the movie.
+
+
+        return hash(title)
+        # I don't know what to do here..... How do I hash this?
+
+    def tokenize(self, title: str) -> str:
+        """
+        Return the first two tokens in the movie title.
+
+        :param title: A full movie title
+        :return: The first two tokens as a string.
         """
 
-        # I don't know what to do here..... How do I hash this?
+        line = str.split(title, " ")
+
+
+
+        return final
+
+    
